899e41442c2626dc2fd891ff11cb66a32ccfb63e
limingming <861673286@qq.com>
2017-05-23 13:03:28 +0800

    external_sites.py

--- chapter3/uniquewords1.py
+++ chapter3/uniquewords1.py
@@ -8,6 +8,7 @@ for filename in sys.argv[1:]:
         for word in line.lower().split():
             word = word.strip(strip)
             if len(word) > 2:
+                # can not words[word] += 1 because this will raise a KeyError exception the first time a new word is encountered
                 words[word] = words.get(word,0) + 1
 for word in sorted(words):
     print("'{0}' occurs {1} times".format(word,words[word]))
