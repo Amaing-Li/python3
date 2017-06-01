04e93eae10da1ef24aeea8cc1606a3aef8d70ed1
ali <amaing@aliyun.com>
2017-06-01 20:57:15 +0800

    comment

--- chapter4/make_html_skeleton.py
+++ chapter4/make_html_skeleton.py
@@ -4,6 +4,7 @@ import \
 
 COPYRIGHT_TEMPLATE = "Copyright (c) {0} {1}. All rights reserved."
 STYLESHEET_TEMPLATE = ('link rel="stylesheet" type="text/css" media="all" href="{0}" />\n')
+# just as it name says, stylesheet is a file(sheet) that controls the html style
 HTML_TEMPLATE = """<?xml version="1.0"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" \
 "html://www.w3.org/TR/xhtml1/DTD/xhtml-strict.dtd">
@@ -23,24 +24,29 @@ HTML_TEMPLATE = """<?xml version="1.0"?>
 """
 
 
-class CancelledError(Exception): pass
+class CancelledError(Exception): pass  # user defined exception, extends Exception
 
 
+# this method stands for the hole thought structure
+# if a process is complex enough or used commonly, then make it a variable or method
+# it is better to think the problem forward and backward and write in whichever order you like
 def main():
     information = dict(name=None, year=datetime.date.today().year, filename=None, title=None, description=None,
-                       keywords=None, stylesheet=None)
+                       keywords=None, stylesheet=None)  # dictionary just like a map in java and scala,
     while True:
         try:
             print("\nMake HTML Skeleton\n")
             populate_information(information)  # update information
-            make_html_skeleton(**information)
-        except CancelledError:
+            make_html_skeleton(**information)  # use the information to make the skeleton
+        except CancelledError:  # user defined exception to control work flow
             print("Cancelled")
-        if (get_string("\nCreate another (y/n)?", default="y").lower() not in {"y", "yes"}):
+        if (get_string("\nCreate another (y/n)?", default="y").lower() not in {"y",
+                                                                               "yes"}):  # loop to generate another html file
             break
 
 
 def populate_information(information):
+    # def get_string(message, name="string", default=None, minimum_length=0, maximum_length=80)
     name = get_string("Enter your name (for copyright)", "name", information["name"])
     if not name:
         raise CancelledError()
@@ -75,36 +81,38 @@ def make_html_skeleton(year, name, title, description, keywords, stylesheet, fil
     copyright = COPYRIGHT_TEMPLATE.format(year, xml.sax.saxutils.escape(name))
     title = xml.sax.saxutils.escape(title)
     description = xml.sax.saxutils.escape(description)
-    keywords = ",".join([xml.sax.saxutils.escape(k) for k in keywords]) if keywords else ""
+    keywords = ",".join([xml.sax.saxutils.escape(k) for k in
+                         keywords]) if keywords else ""  # conditional expression,  embedded is a list comprehension
     stylesheet = STYLESHEET_TEMPLATE.format(stylesheet) if stylesheet else ""
-    html = HTML_TEMPLATE.format(**locals())
+    html = HTML_TEMPLATE.format(**locals())  # mapping uppacking
 
-    fh = None
+    fh = None  # file handler
     try:
-        fh = open(filename, "w", encoding="utf-8")
+        fh = open(filename, "w", encoding="utf-8")  # w: write
         fh.write(html)
     except EnvironmentError as err:
         print("ERROR", err)
     else:
-        print("Saved skeleton", filename)
+        print("Saved skeleton", filename)  # if the try_suite finished normally, this else_suite is executed
     finally:
         if fh is not None:
             fh.close()
 
 
 def get_string(message, name="string", default=None, minimum_length=0, maximum_length=80):
-    message += ": " if default is None else " [{0}]: ".format(default)
+    message += ": " if default is None else " [{0}]: ".format(default)  # if...else... expression
     while True:
         try:
-            line = input(message)
-            if not line:
+            line = input(message)  # get input from console
+            if not line:  # not true, that is false, empty
                 if default is not None:
                     return default
-                if minimum_length == 0:
+                if minimum_length == 0:  # go here, name is None and default is None
                     return ""
                 else:
                     raise ValueError("{0} may not be empty".format(name))
-            if not (minimum_length <= len(line) <= maximum_length):
+            if not (minimum_length <= len(
+                    line) <= maximum_length):  # restrict line's length, python support this format comparison
                 raise ValueError(
                     "{name} must have at least {minimum_length} and at most {maximum_length} characters".format(
                         **locals()))
