6845bf1b1baa54e3fe8936da331f793af7f4d123
ali <amaing@aliyun.com>
2017-06-06 11:11:04 +0800

    heapq

---
+++ chapter5/heapq_example.py
@@ -0,0 +1,14 @@
+# a heap is a binary tree that repects the heap property, which is that
+# the first item (at index position 0) is always the samllest item.
+# each of a heap's subtrees is also a heap
+import heapq
+
+heap = []
+heapq.heappush(heap, (5, "rest"))
+heapq.heappush(heap, (2, "work"))
+heapq.heappush(heap, (4, "study"))
+
+# heapq.heapify(alist)  # to convert a list to a heap
+heapq.heappop(heap)  # the smallest item can be removed
+for x in heapq.merge([1, 3, 5, 8], [2, 4, 7], [0, 1, 6, 8, 9]):
+    print(x, end=" ")  # prints: 0 1 1 2 3 4 5 6 7 8 8 9
